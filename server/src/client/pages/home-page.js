77085aee710ca3368cf17307613fd42718fdce69
Srinaath Ravichandran <sravichandran@deloitte.com>
2018-04-03 20:29:51 -0600

    Homepage update
    Signed-off-by: Srinaath Ravichandran <sravichandran@deloitte.com>

--- server/src/client/pages/home-page.js
+++ server/src/client/pages/home-page.js
@@ -3,8 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <div> I'm Sri's first SSR home component</div>
-      <button onClick = {() => { console.log('Hi there');}}> Press me </button>
+      <div> This is the home component</div>
     </div>
   )
 }
